c6ba515bf29e846aa8b8c065a555f3155bc5adc2
aloukikaditya <aloukikaditya@gmail.com>
2023-05-14 19:28:29 -0400

    first commit

---
+++ mpt7-fintune.py
@@ -0,0 +1,28 @@
+﻿
+from pathlib import Path
+from sklearn.model_selection import train_test_split
+import torch
+from torch.utils.data import Dataset
+from transformers import DistilBertTokenizerFast, DistilBertForSequenceClassification
+from transformers import Trainer, TrainingArguments
+# 1. Prepare dataset
+I
+# 2. Load pretrained Tokenizer, call it with dataset -> encoding
+# 3. Build PyTorch Dataset with encodings
+# 4. Load pretrained Model
+# 5. a) Load Trainer and train it
+#b) or use native Pytorch training pipeline
+model_name = "distilbert-base-uncased"
+
+﻿
+from pathlib import Path
+
+def read_imdb_split(split_dir):
+    split_dir = Path(split_dir)
+    texts = []
+    labels = []
+    for label_dir in ["pos", "neg"]:
+        for text_file in (split_dir / label_dir).iterdir():
+            texts.append(text_file.read_text())
+            labels.append(0 if label_dir == "neg" else 1)
+    return texts, labels
